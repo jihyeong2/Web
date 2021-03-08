156ef1dd749977708cc7657da1aabdc683fcbfd4
Ji-hyeong <qmffn38@gmail.com>
2021-03-08 22:59:45 +0900

    React Habit Tracker Update & React Youtube Project Update

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -1,9 +1,22 @@
 import React, { useState,useEffect } from 'react';
-import './app.css';
+import styles from './app.module.css';
+import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
 function App() {
   const [videos,setVideos] = useState([]);
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBxGqnbZDYVu-Slfu2reIe6fTr81-8cr_M`, requestOptions)
+      .then(response => response.json())
+      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
+      .then(items => setVideos(items))
+      .catch(error => console.log('error', error));
+  };
 
   useEffect(()=>{
     const requestOptions = {
@@ -17,8 +30,12 @@ function App() {
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   },[]);
+  //  빈 리스트를 전달하면 처음 마운트 될 때만 실행
   return (
-    <VideoList videos={videos}/>
+    <div className={styles.app}>
+      <SearchHeader onSearch={search}/>
+      <VideoList videos={videos}/>
+    </div>
   );
 }
 
