6449d111507ac4537c3e1d0e3bb388517c416e4e
Ji-hyeong <qmffn38@gmail.com>
2021-03-08 23:34:17 +0900

    React youtube service

--- react-basic/youtube/src/app.jsx
+++ react-basic/youtube/src/app.jsx
@@ -3,32 +3,17 @@ import styles from './app.module.css';
 import SearchHeader from './components/search_header/search_header';
 import VideoList from './components/video_list/video_list';
 
-function App() {
+function App({youtube}) {
   const [videos,setVideos] = useState([]);
   const search = query => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyBxGqnbZDYVu-Slfu2reIe6fTr81-8cr_M`, requestOptions)
-      .then(response => response.json())
-      .then(result => result.items.map(item => ({...item, id: item.id.videoId})))
-      .then(items => setVideos(items))
-      .catch(error => console.log('error', error));
+    youtube
+    .search(query)
+    .then(videos => setVideos(videos));
   };
-
   useEffect(()=>{
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
-    };
-    
-    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyBxGqnbZDYVu-Slfu2reIe6fTr81-8cr_M",
-    requestOptions)
-      .then(response => response.json())
-      .then(result => setVideos(result.items))
-      .catch(error => console.log('error', error));
+    youtube
+    .mostPopular()
+    .then(videos => setVideos(videos));
   },[]);
   //  빈 리스트를 전달하면 처음 마운트 될 때만 실행
   return (
